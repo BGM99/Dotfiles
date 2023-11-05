5b839a93f47ea5811f074ec59f815428c4b29abf
Frost-Phoenix <67cyril6767@gmail.com>
2023-11-05 11:40:44 +0100

    first commit

---
+++ modules/home/git.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }: 
+{
+  programs.git = {
+    enable = true;
+    
+    userName = "BGM99";
+    userEmail = "45606744+BGM99@users.noreply.github.com";
+    
+    extraConfig = { 
+      init.defaultBranch = "main";
+      credential.helper = "store";
+    };
+  };
+
+  # home.packages = [ pkgs.gh pkgs.git-lfs ];
+}
