48c9019cf6c7269b5ef591e04c720cb4660fc1a6
BGM99 <45606744+BGM99@users.noreply.github.com>
2026-08-01 00:54:15 +0200

    Fix gnome keyring

--- modules/core/security.nix
+++ modules/core/security.nix
@@ -1,7 +1,11 @@
-{ ... }:
-{
-  security.rtkit.enable = true;
-  security.sudo.enable = true;
-  # security.pam.services.swaylock = { };
-  security.pam.services.hyprlock = { };
+{ ... }: {
+  security = {
+    rtkit.enable = true;
+    sudo.enable = true;
+
+    pam.services = {
+      swaylock.enableGnomeKeyring = true;
+      hyprlock.enableGnomeKeyring = true;
+    };
+  };
 }
